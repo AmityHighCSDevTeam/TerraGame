0e478fb2dce21c00209d084b22ce698883fa03b6
SergeySave <sergeysav.nn@gmail.com>
2016-11-16 15:13:33 -0500

    adds LibGDX to the project

---
+++ desktop/src/org/amityregion5/terragame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.amityregion5.terragame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.amityregion5.terragame.TerraGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TerraGame(), config);
+	}
+}
