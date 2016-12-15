897de0dfb01290ab7fc5f2d1bd9c7af01551e602
Derpanieux <kevint2001@gmail.com>
2016-12-14 19:19:12 -0500

    Does things that sergey wanted to have done.

--- desktop/src/org/amityregion5/terragame/desktop/DesktopLauncher.java
+++ desktop/src/org/amityregion5/terragame/desktop/DesktopLauncher.java
@@ -1,5 +1,4 @@
 package org.amityregion5.terragame.desktop;
-
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.amityregion5.terragame.TerraGame;
@@ -7,6 +6,7 @@ import org.amityregion5.terragame.TerraGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//config.
 		new LwjglApplication(new TerraGame(), config);
 	}
 }
